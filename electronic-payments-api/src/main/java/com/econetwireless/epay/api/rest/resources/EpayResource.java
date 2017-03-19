500d894af97cda7bdc03ad5c747099cac466009c
EconetWireless <sysdev@econet.co.zw>
2017-03-19 20:57:30 +0200

    First commit of the bug project

--- electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
+++ electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("resources/services")
 public class EpayResource {
 
-    @Autowired
     private EpayRequestProcessor epayRequestProcessor;
 
 
